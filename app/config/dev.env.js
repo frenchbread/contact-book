ee08480dc1801cf042083b01a965170b8be053bd
Damir Mustafin <damir.mustafin@protonmail.ch>
2017-11-27 20:13:39 +0300

    Fix env variables for production

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -1,8 +1,6 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
-var config = require('./config.js')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_HOST: `"${config.api_host}"`
+  NODE_ENV: '"development"'
 })
--- app/config/prod.env.js
+++ app/config/prod.env.js
@@ -1,3 +1,6 @@
+var config = require('./config.js')
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_HOST: `"${config.api_host}"`
 }
